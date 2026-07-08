1b307eb495a0af68ccb205bcd2b732060c758118
zainllw0w <pilimcenu270795@gmail.com>
2026-07-09 00:37:55 +0300

    Fix twocomms brand name in privacy policy

--- twocomms/management/tests_ig_privacy_policy.py
+++ twocomms/management/tests_ig_privacy_policy.py
@@ -15,7 +15,7 @@ class InstagramBotPrivacyPolicyTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertNotIn("/login/", response.get("Location", ""))
-        self.assertContains(response, "Privacy Policy for the Tucoms Instagram Direct Bot")
+        self.assertContains(response, "Privacy Policy for the twocomms Instagram Direct Bot")
         self.assertContains(response, "DIRECT_BOT")
         self.assertContains(response, "2120980214971807")
         self.assertContains(response, "https://www.instagram.com/twocomms/")
