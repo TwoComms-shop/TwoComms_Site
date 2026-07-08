bc4e9969dc67e8b0d42f68d9c012053f8ec2140b
zainllw0w <pilimcenu270795@gmail.com>
2026-07-09 00:35:38 +0300

    Clarify Instagram bot app identity in privacy policy

--- twocomms/management/tests_ig_privacy_policy.py
+++ twocomms/management/tests_ig_privacy_policy.py
@@ -16,6 +16,10 @@ class InstagramBotPrivacyPolicyTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertNotIn("/login/", response.get("Location", ""))
         self.assertContains(response, "Privacy Policy for the Tucoms Instagram Direct Bot")
+        self.assertContains(response, "DIRECT_BOT")
+        self.assertContains(response, "2120980214971807")
+        self.assertContains(response, "https://www.instagram.com/twocomms/")
+        self.assertContains(response, "Gemini 3.1 Flash")
         self.assertContains(response, "data deletion")
         self.assertContains(response, "cooperation@twocomms.shop")
 
